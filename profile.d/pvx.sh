745cb7316f359ee2191dfeb5ae8fffbae70f6beb
Romain Collombar <romain.collomar@provexi.fr>
2013-06-24 11:44:03 +0200

    Add alias pvxapp

--- profile.d/pvx.sh
+++ profile.d/pvx.sh
@@ -1,35 +1,15 @@
 # ==================================
-#     VARIABLES D'ENVIRONNEMENT
-# ==================================
-export GUNICORN=~/gunicorn
-export GUNICORN_PYTHON=$GUNICORN/bin/python
-export GUNICORN_CORE=$GUNICORN/sandboxes/PvxCoreApplication
-export GUNICORN_LIB=$GUNICORN/lib/python2.7
-export GUNICORN_PYTHON=$GUNICORN/bin/python
-export DOWNLOAD=~/download
-export OLD_PVX_PATH=~/zope212
-export ITOOLS_PKG=itools-0.50.5
-
-# ==================================
 #     ALIAS
 # ==================================
-# OLD PLATEFORME
-alias pvxcore='cd ~/zope212/sandboxes/PvxCoreApplication/'
-alias runzope='cd ~/zope212/zeo-client0; ./bin/runzope'
-alias runzeo='cd ~/zope212/zeo-server; ./bin/zeoctl start'
-# NEW PLATEFORME
-alias runpvxapp='cd $GUNICORN; source bin/activate; cd $GUNICORN_CORE; python pvxapp.py $GUNICORN'
-alias rungunicorn='cd $GUNICORN; source bin/activate; cd $GUNICORN_CORE; gunicorn -t 0 -c $GUNICORN/etc/gunicorn.py pvxapp:app'
-alias pvxappcore='cd $GUNICORN_CORE'
-alias pvxtree='pvxappcore; vi -c :Ex'
 # SSH
 alias sithaque='ssh ithaque'
 alias samance='ssh amance'
 alias slangres='ssh langres'
 alias schaource='ssh chaource'
-# DEV
-alias del_pyc='find $GUNICORN_CORE -name "*.pyc" -exec rm {}  \;'
-alias del_swp='find $GUNICORN_CORE -name "*.swp" -exec rm {}  \;'
+alias tail_ith_sql="sithaque 'sudo tail -f -n 100 /usr/local/pgsql/data/pg_log/\`sudo ls -tr /usr/local/pgsql/data/pg_log/|tail -n 1\`' |lwatch -i-"
+alias tail_ith_select="sithaque 'sudo tail -f -n 200 /usr/local/pgsql/data/pg_log/\`sudo ls -tr /usr/local/pgsql/data/pg_log/|tail -n 1\`' |ccze -A |grep -i select"
+alias maj_wiki_temple="ssh pvx@temple 'bash -c \"cd wiki.provexi.fr/PvxCoreApplication && git fetch origin && git rebase origin/next && cd doc; make html;\"'"
+
 # GIT
 alias pvxcore_master='cd ~/zope212/sandboxes/master/PvxCoreApplication'
 alias pvxcore_next='cd ~/zope212/sandboxes/next/PvxCoreApplication'
@@ -41,7 +21,21 @@ alias clean_master='mv ~/zope212/sandboxes/master/patchs/* ~/zope212/sandboxes/m
 alias clean_next='mv ~/zope212/sandboxes/next/patchs/* ~/zope212/sandboxes/next/applied_patchs'
 alias ll_master='ll ~/zope212/sandboxes/master/patch'
 alias ll_next='ll ~/zope212/sandboxes/next/patch'
-alias tail_ith_sql="sithaque 'sudo tail -f -n 100 /usr/local/pgsql/data/pg_log/\`sudo ls -tr /usr/local/pgsql/data/pg_log/|tail -n 1\`' |lwatch -i-"
-alias tail_ith_select="sithaque 'sudo tail -f -n 200 /usr/local/pgsql/data/pg_log/\`sudo ls -tr /usr/local/pgsql/data/pg_log/|tail -n 1\`' |ccze -A |grep -i select"
 alias maj_wiki="source ~/zope212/bin/activate; pvxcore; cd doc; make clean; make html"
-alias maj_wiki_temple="ssh pvx@temple 'bash -c \"cd wiki.provexi.fr/PvxCoreApplication && git fetch origin && git rebase origin/next && cd doc; make html;\"'"
+
+
+# ==================================
+#              PVXAPP
+# ==================================
+export PVXAPP=~/pvxapp
+export PVXAPP_PYTHON=$PVXAPP/bin/python
+export PVXAPP_CORE=$PVXAPP/sandboxes/PvxCoreApplication
+export PVXAPP_LIB=$PVXAPP/lib/python2.7
+export PVXAPP_DOWNLOAD=$PVXAPP/download
+export ITOOLS_PKG=itools-0.50.5
+alias runpvxapp='cd $PVXAPP; source bin/activate; cd $PVXAPP_CORE; python pvxapp.py $PVXAPP'
+alias pvxappcore='cd $PVXAPP_CORE'
+alias rungunicorn='cd $PVXAPP; source bin/activate; cd $PVXAPP_CORE; gunicorn -t 0 -c $GUNICORN/etc/gunicorn.py pvxapp:app'
+alias pvxtree='pvxappcore; vi -c :Ex'
+alias delpyc_pvxapp='find $PVXAPP_CORE -name "*.pyc" -exec rm {}  \;'
+alias delswp_pvxapp='find $PVXAPP_CORE -name "*.swp" -exec rm {}  \;'
