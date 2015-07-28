ebacd8fb0b036de0bc7a41d397f50045efa54ae1
rcr <rcr@provexi.fr>
2015-07-28 14:37:52 +0200

    Fix mynewtr

--- profile.d/perso.sh
+++ profile.d/perso.sh
@@ -8,23 +8,19 @@ alias runepicurom='~/bin/google_appengine/dev_appserver.py ~/sandboxes/epicurom/
 alias epicurom='cd ~/sandboxes/epicurom'
 alias mytr="vi $THIS_WEEK"
 alias mytrdir="cd $TR_DIR"
-alias mynewtr="newtr_func "
+alias mynewtr="create_newtr "
 
-newtr_func() {
+function create_newtr {
     if [ -n "$1" ]
     then
-        trdir
-        mv this_week.txt 14_$1_tr.txt
-        cp pattern.txt this_week.txt
+        mytrdir
+        if [ -a this_week.txt ]
+        then
+            rm this_week.txt
+        fi
+        cp pattern.txt 15_$1_tr.txt
+        ln -s 15_$1_tr.txt this_week.txt
     else
         echo Usage: newtr week-1_name
     fi
-    exit 1
 }
-
-alias mytest="test_func "
-
-test_func() {
-    echo $1
-}
-
